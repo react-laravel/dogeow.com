33ec15269f0bb8b7e47b7d581a0e8de6f5db7bc7
doge <doge@gmail.com>
2025-03-26 21:28:41 +0800

    不要头部

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import Header from "./layouts/Header";
 import Main from "./layouts/Main";
 import Footer from "./layouts/Footer";
 
@@ -25,7 +24,6 @@ export default () => {
       id="app"
       className="min-w-screen min-h-screen flex flex-col justify-between bg-random bg-cover bg-center transition-opacity duration-700 text-sm text-gray-200"
     >
-      <Header />
       <Main doings={info.doings} />
       <Footer links={info.links} />
     </div>
--- src/layouts/Header.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export default () => (
-  <header className="flex justify-between">
-    <div className="p-2">{process.env.REACT_APP_NAME}</div>
-    <div className="p-2">小李世界</div>
-  </header>
-);
